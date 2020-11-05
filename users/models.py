9323fa062071a9322f8b5a4eb3a39735806be00c
kilewa <kilewageorge230@gmail.com>
2020-11-05 12:33:31 +0300

    login and logout details

---
+++ users/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from PIL import Image
+from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True,blank=True)
+    bio = HTMLField(max_length=300,default="No bio")
+    profile_photo = models.ImageField(
+        default='default.jpg', upload_to='profile_pics')
+    contact = models.CharField(max_length=50,default='0700034034')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        '''
+        Saves profile instance to db
+        '''
+        self.save()
+
+    @classmethod
+    def get_by_id(cls,id):
+        profile = Profile.objects.get(user = id)
+        return profile
+    
+    @classmethod
+    def filter_by_id(cls,id): 
+        profile = Profile.objects.filter(user = id).first()
+        return profile
+
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+    @receiver(post_save, sender=User) 
+    def save_profile(sender,instance,**kwargs):
+        instance.profile.save()  
